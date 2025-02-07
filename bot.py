6e00633a8021b1d3af62eeb65c9489a49d3bb1f2
liyacl <zdorovo.lisichkina@gmail.com>
2025-02-08 00:53:52 +0800

    add bot token to bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from handlers_new import router
 from middlewares import LoggingMiddleware
+from config import BOT_TOKEN
 
-# bot = Bot(token=TOKEN)
-bot = Bot(token="7433278727:AAF01LEDHtXm8lscAZ9WkhStKPEafQmuFSE")
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 dp.include_router(router)
 dp.message.middleware(LoggingMiddleware())
